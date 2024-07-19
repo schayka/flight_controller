6ceca7af6ecf561c80820067d82e3c62797a7f49
Станислав Чайка <stas@MacBook-Air-Stanislav.local>
2024-07-19 19:20:29 +0300

    add test motors function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 /*
-* Arduino Wireless Communication Tutorial
-*       Example 1 - Receiver Code
-*
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-*
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+* Arduino Flight Controller
+ *
+ * /dev/ttyUSB0
+ *
 */
 
 #include <SPI.h>
@@ -15,7 +13,7 @@ RF24 radio(7, 10); // CE, CSN
 
 const byte address[6] = "00001";
 
-struct MyData {
+struct TransmitterInData {
     byte throttle;
     byte yaw;
     byte pitch;
@@ -29,16 +27,13 @@ int motor_fr = 9;
 int motor_bl = 5;
 int motor_br = 3;
 
-int counter = 1;
+int motor_gain = 0;
 
-MyData data;
 
-void resetData()
+TransmitterInData data;
+
+void reset_data()
 {
-    //This are the start values of each channal
-    // Throttle is 0 in order to stop the motors
-    //127 is the middle value of the 10ADC.
-
     data.throttle = 0;
     data.yaw = 127;
     data.pitch = 127;
@@ -47,9 +42,56 @@ void resetData()
     data.AUX2 = 0;
 }
 
+void test_motors()
+{
+    if (radio.available()) {
+        radio.read(&data, sizeof(data));
+    }
+    else
+    {
+        reset_data();
+    }
+
+    if(data.throttle > 135)
+    {
+        int val = data.throttle - 135;
+        motor_gain = val * (255 / (255 - 136) );
+        motor_gain = constrain(motor_gain, 0, 255);
+        Serial.print(" throttle > 135 |  ");
+        Serial.println(motor_gain);
+        analogWrite(motor_fl,motor_gain);
+        analogWrite(motor_fr,motor_gain);
+        analogWrite(motor_bl,motor_gain);
+        analogWrite(motor_br,motor_gain);
+    }
+    else
+    {
+        analogWrite(motor_fl,0);
+        analogWrite(motor_fr,0);
+        analogWrite(motor_bl,0);
+        analogWrite(motor_br,0);
+    }
+}
+
+void view_TransmitterInData()
+{
+    Serial.print("throttle : ");
+    Serial.print(data.throttle);
+    Serial.print("/ yaw : ");
+    Serial.print(data.yaw);
+    Serial.print(" / pitch :");
+    Serial.print(data.pitch);
+    Serial.print(" / roll : ");
+    Serial.print(data.roll);
+    Serial.print(" / AUX1 : ");
+    Serial.print(data.AUX1);
+    Serial.print(" / AUX2 : ");
+    Serial.println(data.AUX2);
+}
+
 void setup() {
 
-    resetData();
+    reset_data();
 
     pinMode(motor_fl, OUTPUT);
     pinMode(motor_fr, OUTPUT);
@@ -64,11 +106,7 @@ void setup() {
 
 }
 
-int motor_gain = 0;
-
 void loop() {
-
-
 
     if (radio.available()) {
         Serial.println("radio available");
@@ -77,40 +115,6 @@ void loop() {
         Serial.println("radio unavailable");
     }
 
-    if (radio.available()) {
-        radio.read(&data, sizeof(data));
-        if(data.throttle > 135)
-        {
-            motor_gain = data.throttle / 9;
-            motor_gain = constrain(motor_gain, 0, 255);
-
-            Serial.print(" throttle > 135 |  ");
-            Serial.println(motor_gain);
-            analogWrite(motor_fl,motor_gain);
-            analogWrite(motor_fr,motor_gain);
-            analogWrite(motor_bl,motor_gain);
-            analogWrite(motor_br,motor_gain);
-        }
-        else
-        {
-            analogWrite(motor_fl,0);
-            analogWrite(motor_fr,0);
-            analogWrite(motor_bl,0);
-            analogWrite(motor_br,0);
-        }
-    }
-
-    Serial.print("throttle : ");
-    Serial.print(data.throttle);
-    Serial.print("/ yaw : ");
-    Serial.print(data.yaw);
-    Serial.print(" / pitch :");
-    Serial.print(data.pitch);
-    Serial.print(" / roll : ");
-    Serial.print(data.roll);
-    Serial.print(" / AUX1 : ");
-    Serial.print(data.AUX1);
-    Serial.print(" / AUX2 : ");
-    Serial.println(data.AUX2);
-
+    test_motors();
+    view_TransmitterInData();
 }
