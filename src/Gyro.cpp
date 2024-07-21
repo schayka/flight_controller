2f018c03ab7b632cb91b278ee8c7587dd883c5ea
Stanislav <alex.baluc@gmail.com>
2024-07-21 17:13:30 +0300

    add commets to Gyro lib

--- src/Gyro.cpp
+++ src/Gyro.cpp
@@ -4,9 +4,13 @@
 
 void Gyro::setup()
 {
+    // Начало передачи на устройство с адресом 0x68 (MPU-6050)
     Wire.beginTransmission(0x68);
+    // Адрес регистра питания (PWR_MGMT_1)
     Wire.write(0x6B);
+    // Запись 0 в регистр (разбудить MPU6050)
     Wire.write(0x00);
+    // Завершение передачи
     Wire.endTransmission();
 
     calibrate();
@@ -14,31 +18,35 @@ void Gyro::setup()
 
 void Gyro::update_data(bool calibration)
 {
-    // start I2C communication
+    // Начало передачи на устройство с адресом 0x68 (MPU-6050)
     Wire.beginTransmission(0x68);
-    // turn on low pass filter
+    // Регистр конфигурации (CONFIG (адрес 0x1A) )
     Wire.write(0x1A);
+    // Установить фильтр с частотой среза 10 Гц
     Wire.write(0x05);
     // finish I2C communication
     Wire.endTransmission();
 
-    // set the sensitivity scale factor
+    // // Установить диапазон гироскопа на ± 500 °/s
     Wire.beginTransmission(0x68);
+    // Регистр конфигурации гироскопа (GYRO_CONFIG (адрес 0x1B) )
     Wire.write(0x1B);
+    // значение для 500 °/s - 1, в бинарном виде - 00001000, т.е. 8
     Wire.write(0x8);
     Wire.endTransmission();
 
     // access registers storing gyro measurements
     Wire.beginTransmission(0x68);
     Wire.write(0x43);
-    Wire.endTransmission();
+    Wire.endTransmission(false);
 
-    // get the data
+    // get the data from registers storing gyro measurements
 
     Wire.requestFrom(0x68,6);
     GyroX=Wire.read()<<8 | Wire.read();
     GyroY=Wire.read()<<8 | Wire.read();
     GyroZ=Wire.read()<<8 | Wire.read();
+    // Full Scale Range: ± 500 °/s, LSB Sensitivity: 65.5 LSB/°/s
     RateRoll= ( (float)GyroX/65.5 ) - ( calibration ? 0 : RateCalibrationRoll );
     RatePitch=( (float)GyroY/65.5 ) - ( calibration ? 0 : RateCalibrationPitch );
     RateYaw=( (float)GyroZ/65.5 ) - ( calibration ? 0 : RateCalibrationYaw );
